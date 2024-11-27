4d38332ac6a6d0326017b5abae9c931689b0f5cf
SagitariusC <104857119+SagitariusC@users.noreply.github.com>
2024-11-26 21:03:39 -0300

    Update Lote.java
    Adicionado a documentação com javadoc.

--- src/Lote/Lote.java
+++ src/Lote/Lote.java
@@ -4,6 +4,15 @@ import java.util.*;
 import java.io.Serializable;
 import animais.animais.*;
 
+/**
+ * Essa classe foi criada para armazenar, deletar e visualizar objetos de classes diferentes,  no contexto 
+ * são as classes Suino, Bovino ou Ave. Além de associar o código de indetificação a classe animal. 
+ * Inicialmente foi desenvolvida pelo Willian, mas alterada pelo Davi devido as necessidades para o 
+ * o programa principal.
+ * @author Davi Morangoni
+ * @author Willian Gonçalves
+ */
+
 public class Lote<T> implements Iterable<T>, Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -11,27 +20,65 @@ public class Lote<T> implements Iterable<T>, Serializable {
     private int cdlote; 
     private int tipoAnimal; 
     private int removerLote = 0;
+
    
+    /**Construtor da classe que cria um array para adição dos objetos.
+     * @author Willian Gonçalves
+     */
     public Lote() {
         lista = new ArrayList<>(); 
     }
 
+    
+    /**Método que atribui um código de identificação ao lote.
+     * @author Davi Morangoni
+     * @param idLote
+     */
     public void setIdLote(int idLote) {
         this.cdlote = idLote;
     }
 
+    
+    /**Método que retorna o código de identificação do lote
+     * @author Davi Morangoni
+     * @return int - código identificação do lote
+     */
     public int getIdLote() {
         return cdlote;
     }
 
+
+    /**Método que atribui o tipo de animal que o lote irá possuir.
+     * 1-Bovino
+     * 2-Ave
+     * 3-Suino
+     * @author Davi Morangoni
+     * @param tipoAnimal
+     */
     public void setTipoAnimal(int tipoAnimal) {
         this.tipoAnimal = tipoAnimal;
     }
 
+
+    /**Método que retorna o tipo do animal.
+     * 1-Bovino
+     * 2-Ave
+     * 3-Suino
+     * @author Davi Morangoni
+     * @return int - tipo do animal 
+     */
     public int getTipoAnimal() {
         return tipoAnimal;
     }
 
+
+    /**Método que adiciona um objeto ao lote de acordo com o tipo de animal identificado e o código de identificaçao do lote.
+     * @author Davi Morangoni
+     * @author Willian Gonçalves
+     * @param objeto
+     * @param tipoAnimal
+     * @param IDlote
+     */
     public void adicionarLote(T objeto, int tipoAnimal, int IDlote) {
 
         if (this.tipoAnimal != tipoAnimal) {
@@ -56,6 +103,12 @@ public class Lote<T> implements Iterable<T>, Serializable {
         System.out.println("Animal cadastrado com sucesso!");
     }
 
+
+    /**Método que remove um lote da lista e seta o lote do objeto como 0
+     * @author Davi Morangoni
+     * @author Willian Gonçalves
+     * @param IDlote
+     */
     public void removerLote(int IDlote) {
         lista.removeIf(elemento -> {
             if (elemento instanceof Animal && ((Animal) elemento).getIDlote() == IDlote) {
@@ -73,6 +126,13 @@ public class Lote<T> implements Iterable<T>, Serializable {
         }
     }
 
+
+    /**Método que remove o animal, especificado pelo código dele, do lote
+     * @author Davi Morangoni
+     * @author Willian Gonçalves
+     * @param IDanimal
+     * @return boolean - se o animal foi deletado
+     */
     public boolean removerAnimalote(int IDanimal) {
         boolean encontrado = false;
         Iterator<T> iterator = lista.iterator();
@@ -88,6 +148,12 @@ public class Lote<T> implements Iterable<T>, Serializable {
         return encontrado;
     }
 
+
+    /**Método que imprime alguns atributos do objeto que está armazenado no lote.
+     * O atributos são o código de indentifcação do lote e do objeto, além de identificar o tipo do animal armazenado.
+     * @author Davi Morangoni
+     * @author Willian Gonçalves
+     */
     public void imprimirLotes() {
         if (lista.isEmpty()) {
             System.out.println("Lote: " + cdlote + " está vazio.");
@@ -106,7 +172,15 @@ public class Lote<T> implements Iterable<T>, Serializable {
             }
         }
     }
+    
 
+    /**Método que retorna um lote de acordo com o código identificador do lote.
+     * Inicialmente o lote que o Willian fez tinha a ideia de armazenar o ojeto e associar o código do lote ao objeto, por
+     * isso tem esse método, pois um único objeto do tipo lote teria animais com diferentes códigos de lote
+     * (seriam de lotes diferentes). Mas, foi planejado que cada lote teria apenas um tipo de animal armazenado.
+     * @author Willian Gonçalves
+     * @param IDlote
+     */
     public void imprimirLote(int IDlote) {
         int verificaLote = 0;
         System.out.println("Lote:          Tipo Animal:         ID Animal:");
@@ -128,6 +202,10 @@ public class Lote<T> implements Iterable<T>, Serializable {
         }
     }
 
+
+    /**Método que retorna o tamanho do lote (quantidade de elementos no lote).
+     * @return int tamanho do lote
+     */
     public int tamanho() {
         if (lista != null) {
             return lista.size();
@@ -136,6 +214,11 @@ public class Lote<T> implements Iterable<T>, Serializable {
         }
     }
 
+   
+    /**Retorna um iterador para os objetos armazenado no lote.
+     * Com o iterador é possível percorrer os objetos do lote.
+     * @autho Willian Gonçalves
+     */
     @Override
     public Iterator<T> iterator() {
         return lista.iterator(); 
